2abc7430c7d96ce3bbe22c59bd1551a8428291c1
DavidWProject <davidw.sbu@gmail.com>
2019-03-08 11:45:20 -0500

    Added a safety check for exiting event creation menu
    - Added confirmation when leaving the event creation menu.

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -7,7 +7,8 @@ import { EventRouteActivator } from './events/event-details/event-route-activato
 
 export const appRoutes:Routes = [
     // If angular hits the /new path it will direct there, if we put that route first. If not it will try to match paths with events/:id if we put the events/new route below events/:id
-    { path: 'events/new', component: CreateEventComponent },
+    { path: 'events/new', component: CreateEventComponent,
+      canDeactivate: ['canDeactivateCreateEvent'] },
     { path: 'events', component: EventsListComponent },
     { path: 'events/:id', component: EventDetailsComponent, 
       canActivate: [EventRouteActivator] },
